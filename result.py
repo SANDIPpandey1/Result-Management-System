ec7d3c4f9d184816bd7b6b8ab62bd8cf475e17a3
shrishaaa <110020207+shrishaaa@users.noreply.github.com>
2025-01-29 23:16:05 +0545

    result

--- result.py
+++ result.py
@@ -13,11 +13,13 @@ class resultClass:
         #=======Title=======
         
         title = Label(self.root, text="Add Student Result Details", font=("goudy old style", 20, "bold"), bg="orange", fg="#262626").place(x=50, y=15, width=1180, height=50)
+        self.var_roll=StringVar()
         self.var_name=StringVar()
         self.var_course=StringVar()
         self.var_marks=StringVar()
         self.var_full_marks=StringVar()
         self.roll_list=[]
+        self.fetch_roll=[]
         
         
         
@@ -27,11 +29,47 @@ class resultClass:
         lbl_marks_ob = Label(self.root, text="Marks Obtained", font=("goudy old style", 20, "bold"), bg="white").place(x=50, y=280)
         lbl_full_marks = Label(self.root, text="Full Marks", font=("goudy old style", 20, "bold"), bg="white").place(x=50, y=340)
         
-        self.txt_student = ttk.Combobox(self.root,textvariable=self.var_course,values=self.course_list,font=("goudy old style", 15, "bold"),state='readonly',justify=CENTER)
-        self.txt_student.place(x=480, y=180, width=200)
+        self.txt_student = ttk.Combobox(self.root,textvariable=self.var_roll,values=self.roll_list,font=("goudy old style", 15, "bold"),state='readonly',justify=CENTER)
+        self.txt_student.place(x=280, y=100, width=200)
         self.txt_student.set("Select")
+        btn_search = Button(self.root, text="Search", font=("goudy old style", 15, "bold"), bg="#03a9f4", fg="white", cursor="hand2",command=self.search).place(x=500, y=100, width=100, height=28)
+        
 
-if __name__ == "__main__":
-    root = Tk()
-    obj = resultClass(root)
-    root.mainloop()
+        txt_name = Entry(self.root, textvariable=self.var_name, font=("goudy old style", 20, "bold"), bg="lightyellow").place(x=280, y=160, width=320)
+        txt_course = Entry(self.root, textvariable=self.var_course, font=("goudy old style", 20, "bold"), bg="lightyellow").place(x=280, y=220, width=320)
+        txt_marks = Entry(self.root, textvariable=self.var_marks, font=("goudy old style", 20, "bold"), bg="lightyellow").place(x=280, y=280, width=320)
+        txt_full_marks = Entry(self.root, textvariable=self.var_full_marks, font=("goudy old style", 20, "bold"), bg="lightyellow").place(x=280, y=340, width=320)
+
+
+        btn_add=Button(self.root,text="Submit",font=("times new roman",15),bg="lightgreen",activebackground="lightgreen",cursor="hand2").place(x=300, y=420,width=120, height=35)
+        btn_clear=Button(self.root,text="Clear",font=("times new roman",15),bg="lightgray",activebackground="lightgreen",cursor="hand2").place(x=430, y=420,width=120, height=35)
+
+    def fetch_roll(self):
+        con=sqlite3.connect(database="rms.db")
+        cur=con.cursor()
+        try:
+            cur.execute("SELECT roll FROM student")
+            rows=cur.fetchall()
+            if len(rows)>0:
+                for row in rows:
+                    self.roll_list.append(row[0])
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to {str(ex)}")
+            
+        def search(self):
+            con=sqlite3.connect(database="RMS.db")
+        cur=con.cursor()
+        try:
+            cur.execute(f"Select name, course from student where roll=?", ('%'+self.var_roll.get()+'%',))
+            rows=cur.fetchone()
+            if row!=None:
+                self.var_name.set(row[0])
+                self.var_course.set(row[1])
+            else:
+                messagebox.showerror("Error", f"No Records Found",parent=self.root)
+        except Exception as ex:
+    
+           if __name__ == "__main__":
+            root = Tk()
+            obj = resultClass(root)
+            root.mainloop()
